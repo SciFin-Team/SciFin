4a4d36dd0bfb4911be67a4d25c46e77b622f7711
Fabien Nugier <fabien.nugier@googlemail.com>
2020-07-21 11:02:06 +0200

    Added .py files for genetic algorithms and market data modules.

---
+++ geneticalg/geneticalg.py
@@ -0,0 +1,10 @@
+# Created on 2020/7/21
+
+# This module is for implementations related to genetic algorithms.
+
+import numpy as np
+import pandas as pd
+from datetime import datetime
+from datetime import timedelta
+import random as random
+import matplotlib.pyplot as plt
---
+++ marketdata/marketdata.py
@@ -0,0 +1,11 @@
+# Created on 2020/7/21
+
+# This module is for importing, transforming and visualizing market data.
+
+import numpy as np
+import pandas as pd
+from datetime import datetime
+from datetime import timedelta
+import random as random
+import matplotlib.pyplot as plt
+
