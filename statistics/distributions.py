211fc0ff3a27f01b28da36d0cbc65818835c73bd
Fabien Nugier <fabien.nugier@googlemail.com>
2020-07-24 17:07:00 +0200

    Added class Poisson to distributions.

--- statistics/distributions.py
+++ statistics/distributions.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 from scipy.special import erf, erfinv
 
 
-support = ['R', 'R+']
+support = ['R', 'R+', 'N']
 
 
 class distribution:
@@ -54,7 +54,7 @@ class distribution:
         self.skewness = skewness
         self.kurtosis = kurtosis
         
-    def set_quantiles(self, median):
+    def set_median(self, median):
         """
         Sets the most commonly used quantiles.
         """
@@ -120,21 +120,27 @@ class Normal(distribution):
         Initilialization function.
         """
         assert(sigma>0)
+
         # Type of distribution
         self.type = 'Normal'
         self.support = 'R'
+        
         # moments
         self.mu = mu
         self.sigma = sigma
         self.set_moments(mean = mu, variance = sigma*sigma, skewness = 0., kurtosis = 3.)
+        
         # quantiles
-        self.set_quantiles(median = mu)
+        self.set_median(median = mu)
+        
         # others
         self.set_mode(mode = mu)
         self.set_MAD(MAD = sigma*np.sqrt(2/np.pi))
         self.set_entropy(entropy = (1/2) * np.log(2*np.pi*np.e*sigma*sigma))
+        
         # name (or nickname)
         self.set_name(name)
+        
         
     def PDF(self, x):
         """
@@ -142,13 +148,11 @@ class Normal(distribution):
         """
         return np.exp(-(np.array(x)-self.mu) * (np.array(x)-self.mu) / (2 * self.sigma * self.sigma)) / (self.sigma * np.sqrt(2 * np.pi))
 
-    
     def CDF(self, x):
         """
         Method implementing the Cumulative Distribution Function (CDF) for the Normal distribution.
         """
         return [(1/2) * (1 + erf((x_el - self.mu) / (self.sigma * np.sqrt(2)))) for x_el in x]
-    
     
     def quantile(self, p):
         """
@@ -158,6 +162,101 @@ class Normal(distribution):
         return self.mu + self.sigma * np.sqrt(2) * erfinv(2*p-1)
     
     
+    
+class Poisson(distribution):
+    """
+    Class implementing the Poisson distribution, inheriting from class distribution.
+    We use a value k_max to set the limit of summation for the entropy calculation.
+    """
+    
+    def __init__(self, lmbda=0., k_max=1000, name=""):
+        """
+        Initilialization function.
+        """
+        assert(lmbda>=0.)
+        assert(isinstance(k_max,int))
+        assert(k_max>0)
+        
+        # Type of distribution
+        self.type = 'Poisson'
+        self.support = 'N'
+        
+        # moments
+        self.lmbda = lmbda
+        self.set_moments(mean = lmbda, variance = lmbda, skewness = 1./np.sqrt(lmbda), kurtosis = 3. + 1./np.sqrt(lmbda))
+        
+        # quantiles
+        self.set_median(median = np.floor(lmbda + 1/3 - 0.02/lmbda))
+        
+        # others
+        self.k_max = k_max
+        self.set_mode(mode = np.floor(lmbda))
+        self.set_entropy(entropy = self.entropy_Poisson(self.lmbda))
+        
+        # name (or nickname)
+        self.set_name(name)
+    
+    
+    def entropy_Poisson(self, lmbda):
+        """
+        Computes the entropy for the Poisson distribution.
+        """
+        tmp_sum = 0.
+        for k in range(self.k_max):
+            contrib = np.power(lmbda,k) * np.log(np.math.factorial(k)) / np.math.factorial(k)
+            if contrib < 1.e-15:
+                tmp_sum += contrib
+                break
+        if k==self.k_max:
+            print("Careful. Sum probably did not converge.")
+        return lmbda * (1-np.log(lmbda)) + np.exp(-lmbda) * tmp_sum
+        
+        
+    def PMF(self, klist):
+        """
+        Method implementing the Probability Mass Function (PMF) for the Poisson distribution.
+        """
+        assert(len(klist)>0)
+        
+        for x in klist:
+            assert(isinstance(x,int))
+        pmf = [np.power(self.lmbda,x) * np.exp(-self.lmbda) / np.math.factorial(x) for x in klist]
+        return pmf
+
+    
+    def CDF(self, klist):
+        """
+        Method implementing the Cumulative Distribution Function (CDF) for the Poisson distribution.
+        """
+        # Checks
+        assert(len(klist)>0)
+        N = len(klist)
+        for x in klist:
+            assert(isinstance(x,int))
+            
+        # Check if k's are consecutive
+        ks_consecutive = True
+        for i in range(N-1):
+            if (klist[i+1] != klist[i]+1):
+                ks_consecutive = False
+                break
+        
+        # Computing the list of elements to sum
+        t = []
+        if ks_consecutive==True:
+            k_range = np.floor(klist)
+            tmp_sum = 1.
+            t.append(tmp_sum)
+            for i in range(1,N,1):
+                tmp_sum += np.power(self.lmbda,k_range[i]) / np.math.factorial(k_range[i])
+                t.append(tmp_sum)
+        else:
+            for i in range(N):
+                tmp_sum = [np.power(self.lmbda,i) / np.math.factorial(i) for i in range(np.floor(k_range[i]))].sum()
+                t.append(tmp_sum)
+        
+        # Completing the calculation
+        return np.exp(-self.lmbda) * np.array(t)
     
     
     
