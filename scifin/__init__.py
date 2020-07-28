50e25e3287d5a921af809e98b748f991afecd035
Fabien Nugier <fabien.nugier@googlemail.com>
2020-07-28 22:56:56 +0200

    Updated name of next version in __init__.py files.

--- scifin/__init__.py
+++ scifin/__init__.py
@@ -1,5 +1,5 @@
 # __init__.py
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 
 from . import geneticalg
 from . import marketdata
--- scifin/marketdata/__init__.py
+++ scifin/marketdata/__init__.py
@@ -1,5 +1,5 @@
 # __init__.py
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 
 """
 The :mod:`scifin.marketdata` module includes methods for market data gathering.
