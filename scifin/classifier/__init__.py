e20dbe4a4676f7e1b2c6c5a6c4ba2fb634271320
Fabien Nugier <fabien.nugier@googlemail.com>
2020-07-30 13:33:03 +0200

    Updating __init__.py files.

--- scifin/classifier/__init__.py
+++ scifin/classifier/__init__.py
@@ -1,3 +1,3 @@
 # __init__.py
-__version__ = "0.0.6"
+__version__ = "0.0.7"
 
--- scifin/geneticalg/__init__.py
+++ scifin/geneticalg/__init__.py
@@ -1,5 +1,5 @@
 # __init__.py
-__version__ = "0.0.6"
+__version__ = "0.0.7"
 
 """
 The :mod:`scifin.geneticalg` module includes methods for genetic algorithms.
