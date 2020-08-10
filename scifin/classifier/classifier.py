9f62a3641949207baa6183c1bb0601f76444b962
Fabien Nugier <fabien.nugier@googlemail.com>
2020-08-10 18:22:24 +0200

    Improved dtw_distance() in classifier.classifier.

--- scifin/classifier/classifier.py
+++ scifin/classifier/classifier.py
@@ -55,9 +55,10 @@ def euclidean_distance(ts1, ts2):
     
     
     
-def dtw_distance(ts1, ts2):
+def dtw_distance(ts1, ts2, window=None):
     """
     Returns the Dynamic Time Warping (DTW) distance between two TimeSeries.
+    A locality constraint can be used by specifying the size of a window.
     
     Parameters
     ----------
@@ -65,6 +66,8 @@ def dtw_distance(ts1, ts2):
       First time series.
     ts2 : TimeSeries
       Second time series.
+    window : int
+      Size of restrictive search window.
       
     Returns
     -------
@@ -75,6 +78,9 @@ def dtw_distance(ts1, ts2):
     -----
       To learn more about DTW, please refer to:
       https://en.wikipedia.org/wiki/Dynamic_time_warping
+      
+      Notice that taking a small window size may lead to
+      a wrong estimate of the real dynamic time warping distance.
     """
     
     # Checks
@@ -86,16 +92,22 @@ def dtw_distance(ts1, ts2):
     # Initializations
     N1 = len(ts1.data.index.tolist())
     N2 = len(ts2.data.index.tolist())
+    if window is not None:
+        assert(isinstance(window, int))
+        w = window
+    else:
+        w = N2
     
     dtw = np.full(shape=(N1+1,N2+1), fill_value=np.inf)
     dtw[0,0] = 0
 
     # Loop
-    for i in range(0,N1,1):
-        for j in range(0,N2,1):
-            cost = (ts1.data.values[i] - ts2.data.values[j])**2
-            dtw[i+1,j+1] = cost + min(dtw[i,j+1], dtw[i+1,j], dtw[i,j])
-
+    for i in range(0, N1, 1):
+        # for j in range(0, N2, 1):
+        for j in range(max(0,int(i-w)), min(N2,int(i+w)), 1):
+            square = (ts1.data.values[i] - ts2.data.values[j])**2
+            dtw[i+1,j+1] = square + min(dtw[i,j+1], dtw[i+1,j], dtw[i,j])
+            
     # Return distance
     return np.sqrt(dtw[N1, N2])
 
