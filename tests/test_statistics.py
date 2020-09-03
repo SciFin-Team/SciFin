8dcf806c9928bd94efaae6b0d0b8af39ac457814
Fabien Nugier <fabien.nugier@googlemail.com>
2020-09-03 11:31:28 +0200

    Wrote unit test test_covariance_to_correlation().

--- tests/test_statistics.py
+++ tests/test_statistics.py
@@ -3,6 +3,9 @@ import sys
 from os import path
 sys.path.append(path.join(path.dirname(__file__), '..'))
 
+# Import from standard library
+import itertools
+
 # Import Unittest
 import unittest
 
@@ -39,9 +42,27 @@ class TestStatistics(unittest.TestCase):
         pass
 
 
-    
-    
-    
+    def test_covariance_to_correlation(self):
+        
+        # Test 1 - Arbitrary covariance matrix converted to correlation matrix
+        self.corr0 = st.covariance_to_correlation(np.array([[1.5, -0.75], [-0.75, 1.5]]))
+        self.compare_corr0 = np.array([[ 1. , -0.5], [-0.5,  1. ]])
+        
+        for i,j in itertools.product(range(self.corr0.shape[0]), range(self.corr0.shape[0])):
+            self.assertAlmostEqual(self.corr0[i,j], self.compare_corr0[i,j])
+        
+        
+        # Test 2 - Random covariance matrix converted to covariance matrix
+        self.corr1 = st.covariance_to_correlation(st.random_covariance_matrix(3, 5))
+        
+        for i,j in itertools.product(range(self.corr1.shape[0]), range(self.corr1.shape[0])):
+            if i==j:
+                self.assertEqual(self.corr1[i,j], 1.)
+            else:
+                self.assertEqual(self.corr1[i,j], self.corr1[j,i])
+                
+        
+        
         
 if __name__ == '__main__':
     unittest.main()
