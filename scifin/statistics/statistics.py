7dd2d3fb9869f14c2dfed7553021e2c0d3ed2883
Fabien Nugier <fabien.nugier@googlemail.com>
2020-09-03 13:22:43 +0200

    Implement eigen_value_vector() in statistics.py.

--- scifin/statistics/statistics.py
+++ scifin/statistics/statistics.py
@@ -9,6 +9,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import seaborn as sns
 
 # Local application imports
 # /
@@ -78,7 +79,7 @@ def covariance_to_correlation(cov):
 
     # Checks
     if cov.shape[0] != cov.shape[1]:
-        raise InputError("Covariance matrix should be a square matrix.")
+        raise ValueError("Covariance matrix must be a square matrix.")
     
     # Compute correlation matrix
     std = np.sqrt(np.diag(cov))
@@ -90,5 +91,52 @@ def covariance_to_correlation(cov):
     
     return corr
 
+
+
+def eigen_value_vector(matrix):
+    """
+    Compute eigen value and eigen vector from a Hermitian matrix.
+    
+    Arguments
+    ---------
+    matrix : numpy.array or list of lists
+      Matrix to extract eigen values and eigen vectors from.
+      
+    Returns
+    -------
+    numpy.array, numpy.array
+      Array of eigen values, array of eigen vectors.
+      
+    Notes
+    -----
+      Function adapted from "Advances in Financial Machine Learning",
+      Marcos López de Prado (2018).
+    """
+    
+    # Convert list of lists to numpy.array (if needed)
+    if isinstance(matrix, list):
+        matrix = np.array(matrix)
+        
+    # Checks
+    # Squared matrix
+    if matrix.shape[0] != matrix.shape[1]:
+        raise ValueError("Input matrix must be a square matrix.")
+    # Hermitian
+    if (matrix.T == np.conj(matrix)).flatten().all() == False:
+        raise ValueError("Input matrix must be Hermitian.")
+    
+    
+    # Compute eigen values and vectors
+    e_val, e_vec = np.linalg.eigh(matrix)
+    
+    # Sort by size
+    indices = e_val.argsort()[::-1]
+    e_val = e_val[indices]
+    e_vec = e_vec[:, indices]
+    
+    # Create diagonal matrix with eigenvalues
+    e_val = np.diagflat(e_val)
+    
+    return e_val, e_vec
 
 
