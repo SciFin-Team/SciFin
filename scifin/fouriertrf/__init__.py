e28df5858f7048d7c811a4d7db0de58b2d23d36b
Fabien Nugier <fabien.nugier@googlemail.com>
2020-09-05 17:33:58 +0200

    Corrected __init__.py files, using () instead of breaks.

--- scifin/fouriertrf/__init__.py
+++ scifin/fouriertrf/__init__.py
@@ -1,3 +1,7 @@
 # __init__.py
 __version__ = "0.1.0"
 __author__ = "Fabien Nugier"
+
+"""
+The :mod:`scifin.fouriertrf` module includes methods for Fourier transforms.
+"""
--- scifin/geneticalg/__init__.py
+++ scifin/geneticalg/__init__.py
@@ -6,10 +6,10 @@ __author__ = "Fabien Nugier"
 The :mod:`scifin.geneticalg` module includes methods for genetic algorithms.
 """
 
-from .geneticalg import Individual, Population, \
-                        generate_random_population, get_generation, roulette, selection, \
-                        pairing, non_adjacent_random_list, mating_pair, get_offsprings, \
-                        mutate_individual, mutation_set, mutate_population, \
-                        next_generation, get_elite_and_individuals, \
-                        fitness_similarity_check, sum_top_fitness, plot_compare_genomes
+from .geneticalg import (Individual, Population,
+                         generate_random_population, get_generation, roulette, selection,
+                         pairing, non_adjacent_random_list, mating_pair, get_offsprings,
+                         mutate_individual, mutation_set, mutate_population,
+                         next_generation, get_elite_and_individuals,
+                         fitness_similarity_check, sum_top_fitness, plot_compare_genomes)
 
