1465d3c0e732ca5f63c4a2efbf348e7a18059c6a
Fabien Nugier <fabien.nugier@googlemail.com>
2020-09-11 15:46:40 +0200

    Added 2 files./

---
+++ __init__.py
@@ -0,0 +1,4 @@
+# __init__.py
+__version__ = "0.1.0"
+__author__ = "Fabien Nugier"
+
---
+++ scifin/neuralnets/neuralnets.py
@@ -0,0 +1,22 @@
+# Created on 2020/8/22
+
+# This module is for neural networks implementations.
+
+
+# Standard library imports
+from datetime import datetime
+
+# Third party imports
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+# Local application imports
+# /
+
+
+#---------#---------#---------#---------#---------#---------#---------#---------#---------#
+
+
+
+
