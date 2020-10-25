bcdfa1236560aaea9e3e14fe9f0dacb396994995
Fabien Nugier <fabien.nugier@googlemail.com>
2020-10-25 10:22:10 +0100

    Added folders for new architecture transition.

---
+++ scifin/structures/__init__.py
@@ -0,0 +1,6 @@
+# __init__.py
+__version__ = "0.1.1"
+
+"""
+The :mod:`scifin.structures` module includes classes composing the main structure of SciFin.
+"""
---
+++ scifin/structures/timeseries.py
@@ -0,0 +1,14 @@
+"""
+The :mod:`scifin.structures.timeseries` module includes classes related to time series.
+"""
+
+# Standard library imports
+from datetime import datetime
+
+# Third party imports
+
+# Local application imports
+
+
+#---------#---------#---------#---------#---------#---------#---------#---------#---------#
+
